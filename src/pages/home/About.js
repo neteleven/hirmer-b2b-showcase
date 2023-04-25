ad1cf2d953595281bc771d30f8981c89476c6b63
Tam Nguyen <tam.nguyen@neteleven.de>
2023-04-25 14:37:34 +0200

    added last landing page and improve some css

--- src/pages/home/About.js
+++ src/pages/home/About.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { useSites } from 'context/sites-provider'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from 'context/auth-provider'
 
 const About = () => {
   const {  currentSite } = useSites()
+  const navigate = useNavigate()
+  const { userTenant: tenant } = useAuth()
   const eckerleUrl = 'https://n.hirmercdn.de/eck/content/teasers/startseite/STS_20230406/20230405-newin-dsk.jpg'
 
-  console.log('currentSite', currentSite);
+  const mappingPageName = {
+    'main': 'hirmer',
+    'HGG': 'hirmer_grosse_grössen',
+    'ECK': 'eckerle'
+  }
+
+  const handleSiteChange = () => {
+    navigate(`/${tenant}/product/${mappingPageName[currentSite]}`)
+  }
 
   function aboutVariant (site) {
     switch (site) {
       case 'ECK':
         return (
-          <div className='home_about__eck flex items-center justify-center'>
+          <div className='home_about-eck flex items-center justify-center' onClick={handleSiteChange}>
             <div className="flex items-center justify-center">
               <img src={eckerleUrl} className="" alt='eckler'/>
             </div>  
@@ -20,7 +32,7 @@ const About = () => {
       
       case 'main':
         return (
-          <div className='home-about-hirmer'>
+          <div className='home-about-hirmer' onClick={handleSiteChange}>
             <div class="home-about-hirmer__teaser"> 
                <img src="https://n.hirmercdn.de/hrm/content/teasers/startseite/STS_20230406/20230405_hir_sts_jacken.jpg" alt="hirmer"/>
             </div>
@@ -30,13 +42,24 @@ const About = () => {
             </div>
           </div>
         )
-
-      
+    
       default:
-        break;
+        return (
+          <div className='home-about-hgg' onClick={handleSiteChange}>
+            <div class="home-about-hgg__teaser">
+              <img src="https://n.hirmercdn.de/hgg/content/teasers/startseite/sts_20230406/20230406-lederjacken-en-dsk.jpg"/>
+            </div>
+
+            <div class="home-about-hgg__teaser">
+              <img src="https://n.hirmercdn.de/hgg/content/teasers/startseite/sts_20230406/20230406-sweatshirts-en-dsk.jpg" />
+            </div>
+
+            <div class="home-about-hgg__teaser">
+              <img src="https://n.hirmercdn.de/hgg/content/teasers/startseite/sts_20230406/20230406-marcopolo-en-dsk.jpg" />
+            </div>
+          </div>     
+        )
     }
-
-
   }
 
   return ( 
